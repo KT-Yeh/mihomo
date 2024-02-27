16c267b4d805df1410b3cb2c275335622e7d13b7
KT <xns77477@gmail.com>
2024-02-27 21:50:57 +0800

    fix: memory of chaos ID

--- mihomo/models/player.py
+++ mihomo/models/player.py
@@ -20,7 +20,7 @@ class ForgottenHall(BaseModel):
 
     memory: int = Field(..., alias="level")
     """The progress of the memory (level)"""
-    memory_of_chaos_id: int = Field(..., alias="chaos_id")
+    memory_of_chaos_id: int | None = Field(None, alias="chaos_id")
     """The ID of the memory of chaos (chaos_id)"""
     memory_of_chaos: int | None = Field(None, alias="chaos_level")
     """The progress of the memory of chaos (chaos_level)"""
